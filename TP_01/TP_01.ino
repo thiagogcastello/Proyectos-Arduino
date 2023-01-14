ed5958a6adaf635bcd73468c079697ab9d0c1f1c
thiago gonzalez castello <73041924+thiagogcastello@users.noreply.github.com>
2023-01-14 11:16:48 -0300

    Update TP_01.ino

--- TP_01/TP_01.ino
+++ TP_01/TP_01.ino
@@ -1,39 +1,3 @@
-/*
-================================================================================================
-                            TRABAJO PRACTICO N° 1: Cronómetro Binario    
-
-                                SISTEMA DE PROCESAMIENTO DE DATOS
-                                        UTN FRA - 2022
-
-INTEGRANTES 
-- Castello Thiago
-- Forni Gonzalo
-- Gatti Ignacio Pablo
-- Heredia Facundo.
-
-ENUNCIADO
-Al iniciar la simulacion, todos los led deben comenzar apagados. Cada 1 segundo que transcurra, 
-se deben encender los led de acuerdo al numero que representan.
-
-EJEMPLO:
-- El numero 15 (0000001111) se representaria encendiendo los leds AMARILLO y NARANJA.
-- Al pasar al numero 16 (0000010000), se deben apagar todos los led anteriores y 
-encenderse uno de los leds VERDES. 
-- Notese, en el ejemplo los 0 representan los led apagados y los 1 los led encendidos). 
-   
-Al presionarse el boton START, debe iniciar el cronometro. Volver a presionarlo hace que la 
-secuencia se detenga. (Como si se hubiese presionado pausa).
-Al presionarse el boton RESET, el cronometro debe reiniciarse y arrancar de 0.
-
-TIPS: 
-- Modularizar la función que controla el encendido de los LEDS y de ser posible, 
-todo el código para evitar repetir lineas lo mas posible.
-- Usar millis para controlar el tiempo del contador para que el cambio de los leds encendidos 
-sea perceptible para el ojo humano y documentar cada función creada en el código. 
-- Un breve comentario que diga que es lo que hace esa función y de corresponder, que retorna).
-================================================================================================
-*/
-
 //  =========
 //  > DEFINES
 //  =========
